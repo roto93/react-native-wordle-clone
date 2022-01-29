6fc241f1fb6f86f8d56762d9f1c01320ffa812b5
roto93 <roto93@yahoo.com.tw>
2022-01-29 22:47:28 +0800

    :art: Setup Themes

---
+++ src/Themes.js
@@ -0,0 +1,18 @@
+const Theme = {
+  light: {
+    primary: '#eee',
+    secondary: '#ccc',
+    text: '#333',
+    textBackground: '#fff',
+    black: '#111',
+    white: '#fff'
+  },
+  dark: {
+    primary: '#333',
+    secondary: '#555',
+    text: '#ddd',
+    textBackground: '#111',
+    black: '#111',
+    white: '#fff'
+  }
+}
