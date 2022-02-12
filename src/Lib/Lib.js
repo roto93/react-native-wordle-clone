a084cfdffbcd52b05809f075e966ae7d7d1a5062
roto93 <roto93@yahoo.com.tw>
2022-02-12 14:47:18 +0800

    :zap: Update word APIs

--- src/Lib/Lib.js
+++ src/Lib/Lib.js
@@ -11,3 +11,63 @@ export const RowView = (props) => {
     </View>
   )
 }
+
+export const initAnswerArray = []
+for (let i = 0; i < 6; i++) {
+  initAnswerArray.push([])
+  for (let j = 0; j < 5; j++) {
+    initAnswerArray[i].push({})
+  }
+}
+
+export const initAnswerArrayHard = []
+for (let i = 0; i < 6; i++) {
+  initAnswerArrayHard.push([])
+  for (let j = 0; j < 6; j++) {
+    initAnswerArrayHard[i].push({})
+  }
+}
+
+export const initKeyArray = [
+  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+  ['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'del'],
+]
+
+export const getActivePosition = (arr) => {
+  let activeRowIndex = arr.findIndex(row => JSON.stringify(row) == JSON.stringify([{}, {}, {}, {}, {}]))
+  if (activeRowIndex === -1) activeRowIndex = 5
+  else if (activeRowIndex === 0) activeRowIndex = 0
+  else { activeRowIndex-- }
+  let activeColIndex = arr[activeRowIndex].findIndex(col => col.value === undefined)
+  if (activeColIndex === -1) activeColIndex = arr[0].length
+  else if (activeRowIndex === 0) activeRowIndex = 0
+  else { activeColIndex-- }
+  return { activeRowIndex, activeColIndex }
+}
+
+export const fetchAnswerAPI = async () => {
+  try {
+    const res = await fetch("https://random-words5.p.rapidapi.com/getRandom?wordLength=5", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "random-words5.p.rapidapi.com",
+        "x-rapidapi-key": "e1eead18a6msh78a43a5891821f4p1bf967jsna8e308b60e82"
+      }
+    })
+    const text = await res.text()
+    return text
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+
+export const checkValidWordAPI = async (inputWord) => {
+  try {
+    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`)
+    const json = await res.json()
+    return json[0].word !== undefined
+  } catch (e) {
+    console.log(e.message)
+  }
+}
--- src/screen/MainPage.js
+++ src/screen/MainPage.js
@@ -1,15 +1,116 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import useTheme from '../hooks/useTheme'
+import { useSelector } from 'react-redux';
+import { checkValidWordAPI, fetchAnswerAPI, getActivePosition, initAnswerArray, initAnswerArrayHard, initKeyArray, RowView } from '../Lib/Lib';
+import { memo } from 'react';
 
 const MainPage = () => {
+  const Theme = useTheme()
+  const { isHardMode } = useSelector(state => state.isHardModeReducer)
+  const { isUpperCase } = useSelector(state => state.isUpperCaseReducer)
+  const [answerArray, setAnswerArray] = useState(isHardMode ? initAnswerArrayHard : initAnswerArray);
+  const { activeRowIndex, activeColIndex } = getActivePosition(answerArray)
+
+  const Box = memo(({ rowIndex, col, colIndex }) => {
+    const isActiveRow = rowIndex === activeRowIndex
+    const isActiveCol = colIndex === activeColIndex
+    const isActiveCell = isActiveRow && isActiveCol
+    return (
+      <TouchableOpacity style={[styles.letter_container, { borderColor: isActiveCell ? '#198964' : '#000' }]}>
+        <Text style={[styles.letter, { color: Theme.text }]}>
+          {isUpperCase ? col.value?.toUpperCase() : col.value}
+        </Text>
+      </TouchableOpacity>
+    )
+  }, (prevProps, nextProps) => prevProps === nextProps)
+
+  const Key = memo(({ keyText, index }) => {
+
+    const onKeyPress = async () => {
+      if (keyText === 'enter') {
+        const guess = answerArray[activeRowIndex].map(item => item.value).join('')
+        const isValidWord = await checkValidWordAPI(guess)
+        if (isValidWord) console.log(await fetchAnswerAPI(), guess)
+        return
+      }
+      if (keyText === 'del') {
+        if (activeColIndex === 0) return
+        answerArray[activeRowIndex][activeColIndex - 1] = { value: undefined }
+        setAnswerArray([...answerArray])
+        return
+      }
+      if (activeColIndex === 5) return
+      answerArray[activeRowIndex][activeColIndex] = { value: keyText }
+      setAnswerArray([...answerArray])
+    }
+
+    return (
+      <TouchableOpacity style={styles.key} onPress={onKeyPress}>
+        <Text style={styles.key_text}>
+          {isUpperCase ? keyText.toUpperCase() : keyText}
+        </Text>
+      </TouchableOpacity>
+    )
+  })
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <View style={[styles.container, { backgroundColor: Theme.primary }]}>
+      <View style={styles.answer_container}>
+        {answerArray.map(
+          (row, rowIndex) => <RowView style={{ width: 272, justifyContent: 'space-between', marginBottom: 8 }} key={rowIndex}>
+            {answerArray[rowIndex].map((col, colIndex) => <Box key={colIndex} {...{ rowIndex, col, colIndex }} />)}
+          </RowView>
+        )}
+      </View>
+      <View style={styles.key_container}>
+        {initKeyArray.map(
+          (row, rowIndex) => <RowView key={rowIndex} style={{ width: rowIndex === 0 ? 316 : rowIndex === 1 ? 284 : 336, justifyContent: 'space-between' }}>
+            {initKeyArray[rowIndex].map((keyText, index) => <Key key={index} keyText={keyText} index={index} />)}
+          </RowView>)}
+      </View>
+    </View >
   );
 };
 
 export default MainPage;
 
-const styles = StyleSheet.create({});
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24
+  },
+  answer_container: {
+    marginTop: 32
+  },
+  key_container: {
+    height: 126,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  key: {
+    minWidth: 28,
+    height: 36,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    paddingHorizontal: 8
+  },
+  key_text: {
+    fontSize: 16,
+  },
+  letter_container: {
+    width: 48,
+    height: 48,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 4
+  },
+  letter: {
+    fontSize: 24
+  }
+});
